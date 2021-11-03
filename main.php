ba0b6c5c55cdb65750952d09cbff8d53c75a9124
acai0 <alicecai08@hotmail.com>
2021-11-03 17:07:06 +0100

    Commande.php

--- main.php
+++ main.php
@@ -1,2 +1,13 @@
 <?php
-require_once 'conf/config.ini';
+require_once 'Data';
+//use model\Commande as commande;
+include "model/Commande.php";
+//Récupérer toute les commandes
+echo "/****************************Recupere toute les commandes ********************************/<br>";
+$c = new Commande();
+$requete = $c::select(); 
+
+$lignesC = $requete->get();   
+
+foreach ($lignesC as $c)     
+    echo "Identifiant = $c->id, Nom = $c->nom_client <br>" ;
