1c315d4b766184887156457c9cda063c1c22fe2d
Morphewo <42540123+Morphewo@users.noreply.github.com>
2021-11-03 17:10:39 +0100

    Delete Atelier directory

--- Atelier/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'ClassLoaders/AbstractClassLoader.php';
-require_once 'ClassLoaders/ClassLoader.php';
-
-$loader = new ClassLoaders\ClassLoader('.');
-$loader->register();
-
-$panier = new Model\Panier();
-$panier->viderPanier(1);
