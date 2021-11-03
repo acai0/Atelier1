f933dcf05c2a14f416145ca1bc29e9a6a335ac95
acai0 <alicecai08@hotmail.com>
2021-11-03 17:01:05 +0100

    Commande.php

---
+++ model/Panier.php
@@ -0,0 +1,28 @@
+<?php
+namespace modeles;
+class Panier{
+    private $BDD;
+    
+    public function __construct(){
+        $this->BDD= new Data;
+    }
+    
+        public function getById(){
+    
+        $resultat=array();
+        try{
+            $connexion=$this->BDD->connexionPDO($id);
+            $req=$connexion->prepare("select * from panier where id:id");
+            $req->bindValue(":id", $id, PDO::PARAM_STR);
+            $req->execute();
+    
+            $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e){
+            print "Erreur ! : " . $e->getMessage();
+            die();
+                }
+        return $resultat;
+    }
+    
+    }
