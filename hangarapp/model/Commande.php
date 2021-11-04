a629106715b264fde601756dbf58524242f4ef19
acai0 <92530686+acai0@users.noreply.github.com>
2021-11-04 12:15:16 +0100

    Delete hangarapp directory

--- hangarapp/model/Commande.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace hangarapp\model;
-
-class Commande extends \Illuminate\Database\Eloquent\Model{
-
-    protected $table      = 'commande';  /* le nom de la table */
-    protected $primaryKey = 'id';     /* le nom de la clé primaire */
-    public    $timestamps = false;
-}
